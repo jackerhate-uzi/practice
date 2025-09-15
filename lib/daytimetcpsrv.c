f705e1f0a0dd947eb022ee30254c449d6f233f62
fei <feiguiyang637@gmail.com>
2025-09-16 03:17:12 +0800

    practice server program

---
+++ lib/daytimetcpsrv.c
@@ -0,0 +1,34 @@
+#include "unp.h"
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <time.h>
+
+int main(int argc, char **argv) {
+  int listenfd, connfd;
+  struct sockaddr_in servaddr;
+  char buf[MAXLINE];
+  time_t ticks;
+
+  listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(13);
+
+  Bind(listenfd,(SA *)&servaddr,sizeof(servaddr));
+
+  Listen(listenfd, LISTENQ);
+
+  for (; ; ) {
+    connfd = Accept(listenfd, (SA *)NULL, NULL);
+    printf("Connection accept.Server listening on port: %d\n",ntohs(servaddr.sin_port));
+
+    ticks = time(NULL);
+    snprintf(buf, sizeof(buf), "%.24s\r\n",ctime(&ticks));
+    Write(connfd, buf, strlen(buf));
+
+    Close(connfd);
+  }
+}
--- lib/unp.h
+++ lib/unp.h
@@ -2,37 +2,30 @@
 #define __unp_h
 
 #define MAXLINE 4096
-#define SA struct sockaddr
+#define LISTENQ 1024
 
-#include <netinet/in.h>  /* sockaddr_in{} and another Internet defns */
-#include <sys/socket.h> /* basic socket definitions */
-#include <sys/uio.h>   /*for lovec{} and readv/writev */
+#include <arpa/inet.h> /*inet(3) functions */
+#include <errno.h>
+#include <netinet/in.h> /* sockaddr_in{} and another Internet defns */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h>  /*inet(3) functions */
-#include <unistd.h> 
-#include <errno.h>
-
+#include <sys/socket.h> /* basic socket definitions */
+#include <sys/uio.h>    /*for lovec{} and readv/writev */
+#include <unistd.h>
 
 #ifndef HAVE_BZERO
-#define bzero(ptr,n)   memset(ptr,0,n)
+#define bzero(ptr, n) memset(ptr, 0, n)
 #endif /*bzero*/
 
-void err_quit(const char *,...);
-void err_sys(const char *,...);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#define SA struct sockaddr
+void err_quit(const char *, ...); /*error.c*/
+void err_sys(const char *, ...);  /*error.c*/
+int Socket(int, int, int);        /*wrapsock.c*/
+void Bind(int, const SA *,socklen_t);/*wrapsock.c*/
+void Listen(int, int);/*wrapsock.c*/
+int Accept(int, SA *,socklen_t *); /*wrapsock.c*/
+void Write(int,void *,size_t); /*wrapunix.c*/
+void Close(int); /*wrapunix.c*/
 #endif /* __unp_h_ */
+
